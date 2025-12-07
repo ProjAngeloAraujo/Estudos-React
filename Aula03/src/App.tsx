e82f6ede3bfd9450bc0f99c68d5b3a4d576895c7
Angelo <angelo.souza23@etec.sp.gov.br>
2025-12-07 15:31:03 -0300

    Aula04 'Modal melhor explicado'

--- Aula03/src/App.tsx
+++ Aula03/src/App.tsx
@@ -1,21 +1,33 @@
-import { useState, useEffect } from 'react'
-import { Modal } from './components/modal'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-
-  const [Open, setOpen] = useState(false);
-
-  useEffect( () => {
-    
-  }, [])
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <h2>Mostrar Modal</h2>
-        <button onClick={() => { setOpen(!Open) }}>{ Open ? "Fechar" : "Abrir" }</button>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <Modal isOpen={Open} />
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
